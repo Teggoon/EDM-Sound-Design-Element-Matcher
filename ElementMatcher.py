9bd49f31ec5c14ac2bf0579c8d68d610aa9f8b3a
Haocheng Li <49082800+Teggoon@users.noreply.github.com>
2019-11-21 09:50:40 -0800

    Implemented saving functionality

--- ElementMatcher.py
+++ ElementMatcher.py
@@ -1,33 +1,75 @@
 import json
 import random
 
+
 with open('elements.json', 'r') as file:
     fileRead = file.read().replace('\n', '')
 
 
+#Read JSON data file
 data = json.loads(fileRead)
 
+#number of categories for match-making
 categoryNum = len(data["entries"])
 
-print("Number of categories: " + str(categoryNum))
-
+#Object containing sub-branches
 exceptions = data["exceptions"]
 
+#Writer to file
+savedFileWriter = open("saved_matches.txt","a+");
 
-for i in range(0, categoryNum):
+#List storing current match
+currentMatch = []
 
-    wentIn = 0
-    entryNum = len(data["entries"][i]["elems"])
-    picker = random.randint(0,entryNum - 1)
+userInput = ""
 
-    for key in exceptions.keys():
+#Continue to run while the user wants it
+while userInput != "q":
 
-        if key == data["entries"][i]["elems"][picker]:
-            wentIn = 1
-            entryNum = len(data["exceptions"][key])
-            picker = random.randint(0,entryNum - 1)
-            print(data["entries"][i]["name"] + ": " + key + " - " + data["exceptions"][key][picker]);
-            break;
+    #for each category
+    for i in range(0, categoryNum):
 
-    if wentIn == 0:
-        print(data["entries"][i]["name"] + ": " + data["entries"][i]["elems"][picker]);
+        wentIn = False
+        entryNum = len(data["entries"][i]["elems"])
+        picker = random.randint(0,entryNum - 1)
+        pickedElement = data["entries"][i]["elems"][picker]
+
+        #looping through the keys in the exception list
+        for key in exceptions.keys():
+
+            #if found picked element needing stepping in
+            if key == pickedElement:
+                wentIn = True
+                entryNum = len(data["exceptions"][key])
+                picker = random.randint(0,entryNum - 1)
+                outputString = data["entries"][i]["name"] + ": " + key + " - " + data["exceptions"][key][picker];
+
+
+                print(outputString);
+                currentMatch.append(outputString);
+                break;
+
+        if wentIn == False:
+            outputString = data["entries"][i]["name"] + ": " + pickedElement;
+            print(outputString);
+            currentMatch.append(outputString);
+
+
+    print("\nA match has been generated. Press s to save current match, enter to continue, q to quit");
+
+
+    userInput = input();
+    if userInput == " ":
+        currentMatch = []
+        continue
+    elif userInput == "s":
+        output = "------------------\n\n"
+        output += " \n".join(currentMatch)
+        output += "\n\n"
+        savedFileWriter.write(output)
+        currentMatch = []
+        output = ""
+    elif userInput == "q":
+        print("Exiting program.");
+
+savedFileWriter.close();
